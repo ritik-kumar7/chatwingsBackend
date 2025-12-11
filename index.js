700586ad0be6a785664b64f5f188ead03ee7dc03
ritik_kumar7 <ritik.r9k@gmail.com>
2025-12-11 15:49:08 +0530

    updated

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ const port = process.env.PORT || 4000;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-    origin: "*",
-    credentials: true
+    origin: ["https://chatswings.netlify.app", "http://localhost:5173", "http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(cookieParser());
 
